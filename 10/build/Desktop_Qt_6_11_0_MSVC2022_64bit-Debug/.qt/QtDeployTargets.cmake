88bb089fbc8e50200ffdce7055adfccc8d81c3fa
XDyoroshiku <2276341011@qq.com>
2026-04-21 14:47:11 +0800

    移除build,更新.gitignore

--- 10/build/Desktop_Qt_6_11_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Programming_Qt_FILE E:/Projects/Visual Studio/PPP/10/build/Desktop_Qt_6_11_0_MSVC2022_64bit-Debug/Programming_Qt.exe)
-set(__QT_DEPLOY_TARGET_Programming_Qt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Programming_Qt_RUNTIME_DLLS E:/Qt/6.11.0/msvc2022_64/bin/Qt6Widgetsd.dll;E:/Qt/6.11.0/msvc2022_64/bin/Qt6Guid.dll;E:/Qt/6.11.0/msvc2022_64/bin/Qt6Cored.dll)
