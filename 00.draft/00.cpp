825efa41f45525acafbcc32f79917f61dbe301a4
Sheepyawn <2276341011@qq.com>
2026-04-24 08:42:56 +0800

    add Ex_13 of chapter 3

--- 00.draft/00.cpp
+++ 00.draft/00.cpp
@@ -1,9 +1,25 @@
 import PPP;
 using namespace std;
-import calculator;
-
 
 int main()
 {
-	calculate();
+    cout << "Please enter two integer values." << endl;
+    int val1, val2;
+    cin >> val1 >> val2;
+    if (val1 < val2)
+        cout << val1 << " is smaller than " << val2;
+    else if (val1 > val2)
+        cout << val1 << " is larger than " << val2;
+    else
+        cout << val1 << " equals " << val2;
+    cout << endl;
+    int sum = val1 + val2;
+    int difference = val1 - val2;
+    int product = val1 * val2;
+    int ratio = val1 / val2;
+
+    cout << val1 << " + " << val2 << " == " << sum << endl
+        << val1 << " - " << val2 << " == " << difference << endl
+        << val1 << " * " << val2 << " == " << product << endl
+        << val1 << " / " << val2 << " == " << ratio << endl;
 }
--- 03.exercises/exercises/Ex_13.ixx
+++ 03.exercises/exercises/Ex_13.ixx
@@ -1,6 +1,42 @@
 export module Ex_13;
 
-export void Ex_13()
-{
+export void Ex_13();
 
+import std;
+using namespace std;
+
+vector<int> sieve(const int& n)
+{
+	vector<int> primes{ 2 };
+	if (n < 2)
+	{
+		cerr << "n should be greater than 2." << '\n';
+		return primes;
+	}
+
+	int sqrt_n = int(sqrt(n)) + 1;
+	for (int i = 3; i < n; i += 2)
+	{
+		int size = (int)primes.size();
+		bool is_prime = true;
+		for (int j = 0; j < min(size, sqrt_n); ++j)
+		{
+			if (i % primes[j] == 0)
+			{
+				is_prime = false;
+				break;
+			}
+		}
+		if (is_prime)
+			primes.push_back(i);
+	}
+	return primes;
+}
+
+void Ex_13()
+{
+	vector<int> primes = sieve(100);
+
+	for (size_t i = 0; i < primes.size(); ++i)
+		cout << primes[i] << '\n';
 }
